6d6b18df2da8d4bed294ac873138941253f584b5
master <481933884@qq.com>
2018-09-17 16:42:44 +0800

    实现了save & load（待测试）

--- abstract_reptile.cpp
+++ abstract_reptile.cpp
@@ -20,16 +20,55 @@ void AbstractReptile::save()
     {
         QTextStream out(&file);
         out<<this->currentUrl.toString()<<"\r\n";
+        out<<this->analysisData.pattern()<<"\r\n";
+        out<<this->analysisNextUrl.pattern()<<"\r\n";
+        for(auto i:this->allUrl.keys())
+        {
+            out<<i<<"\r\n";
+        }
+        out<<"\r\n";
+        for(auto i:this->data.keys())
+        {
+            out<<i<<"\r\n";
+        }
     }
     else
     {
         emit IOError(QString("Write file error: open fall."));
     }
+    file.close();
 }
 
 void AbstractReptile::load()
 {
-
+    QFile file("temp.data");
+    if(!file.exists())
+        return;
+    if(file.open(QFile::ReadOnly|QFile::Text))
+    {
+        QTextStream in(&file);
+        QString line=in.readLine();
+        this->currentUrl.setUrl(line);
+        line=in.readLine();
+        this->analysisData.setPattern(line);
+        line=in.readLine();
+        this->analysisNextUrl.setPattern(line);
+        line=in.readLine();
+        while(line!=QString(""))
+        {
+            allUrl.insert(line,1);
+        }
+        line=in.readLine();
+        while(!in.atEnd())
+        {
+            data.insert(line,1);
+        }
+    }
+    else
+    {
+        emit IOError(QString("Read file error: open fall."));
+    }
+    file.close();
 }
 
 void AbstractReptile::work()
--- abstract_reptile.h
+++ abstract_reptile.h
@@ -28,7 +28,7 @@ private:
     QUrl currentUrl;//当前网址
     QByteArray replyData;//当前请求收到的数据
     QMap<QString,int> allUrl;//所有请求过的网址（解析到的QString数据）
-    QMap<QString,int> analysis;//解析到的数据数据
+    QMap<QString,int> data;//解析到的数据数据
     QRegExp analysisData;//数据解析规则
     QRegExp analysisNextUrl;//爬取规则
     QNetworkAccessManager * manager;//用于请求网页
